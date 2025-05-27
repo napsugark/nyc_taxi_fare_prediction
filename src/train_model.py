d46905c88a8abbfe9ce5a3a3c98658d5dac74923
Napsugar Kelemen <napsugar.kelemen@qubiz.com>
2025-05-27 16:48:54 +0300

    feat: Modified code to allign with downloading dataset from Kaggle

--- src/train_model.py
+++ src/train_model.py
@@ -1,20 +1,27 @@
+import os
 import pandas as pd
 from pathlib import Path
 
 import mlflow
 from mlflow.models.signature import infer_signature
+import mlflow.data
 from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.metrics import mean_squared_error, root_mean_squared_error, mean_absolute_error, r2_score
-from results_visuals import plot_and_save_grouped_bar_mlruns_metrics, plot_and_save_best_models_summary
+from src.utils.load_dataset import download_kaggle_competition_data
+from src.utils.results_visuals import plot_and_save_grouped_bar_mlruns_metrics, plot_and_save_best_models_summary
+from src.utils.logging_config import logger
+from datetime import datetime
 
 
-from constants import VERSION, EXPERIMENT_NAME, DATA_PATH, MODEL_TYPE, ALPHA
-from preprocessing import load_data, prepare_features, split_and_preprocess
+from constants import DATASET_DOWNLOAD_PATH, VERSION, EXPERIMENT_NAME, MODEL_TYPE, ALPHA
+from preprocessing import load_data, prepare_features, preprocess_data, split_and_preprocess
 
 def train_and_log_model(X_train, X_test, y_train, y_test, preprocessor, version=VERSION, experiment_name=EXPERIMENT_NAME, model_type=MODEL_TYPE, alpha=ALPHA):
     if model_type == 'Lasso':
+        logger.info(f"Training Lasso model with alpha={alpha}...")
         model = Lasso(alpha=alpha)
     elif model_type == 'LinearRegression':
+        logger.info("Training Linear Regression model...")
         model = LinearRegression()
     else:
         raise ValueError(f"model_type must be 'linear' or 'lasso', not {model_type}")
@@ -29,8 +36,9 @@ def train_and_log_model(X_train, X_test, y_train, y_test, preprocessor, version=
         mae = mean_absolute_error(y_test, y_pred)
         r2 = r2_score(y_test, y_pred)
 
-        print(f"MSE: {mse:.4f}, RMSE: {rmse:.4f}, MAE: {mae:.4f}, R2: {r2:.4f}")
+        logger.info(f"Training results: MSE: {mse:.4f}, RMSE: {rmse:.4f}, MAE: {mae:.4f}, R2: {r2:.4f}")
 
+        logger.info(f"Logging model parameters and metrics to MLflow...")
         feature_names = preprocessor.get_feature_names_out()
         num_features = len(feature_names)
         mlflow.log_param("features", ", ".join(feature_names))
@@ -39,15 +47,22 @@ def train_and_log_model(X_train, X_test, y_train, y_test, preprocessor, version=
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("mae", mae)
         mlflow.log_metric("r2", r2)
+        
+        mlflow.log_input(mlflow.data.from_pandas(pd.DataFrame(X_train), name="X_train"), context="training")
+        mlflow.log_input(mlflow.data.from_pandas(pd.DataFrame(X_test), name="X_test"), context="validation")
+
         if model_type == 'Lasso':
             mlflow.log_param("alpha", alpha)
 
+        logger.info("Saving model coefficients and feature importance...")
         coefs = pd.DataFrame({"feature": feature_names, "coefficient": model.coef_})
         coefs_file = Path(f"data/feature_importance/feature_importance_{version}_f{num_features}.csv")
         coefs.to_csv(coefs_file, index=False)
         mlflow.log_artifact(str(coefs_file))
 
+        logger.info(f"Model coefficients saved to {coefs_file}")
     
+        logger.info("Logging model to MLflow...")
         signature = infer_signature(X_train, model.predict(X_train))
         input_example = X_train[:5]
         mlflow.sklearn.log_model(
@@ -56,10 +71,13 @@ def train_and_log_model(X_train, X_test, y_train, y_test, preprocessor, version=
             signature=signature,
             input_example=input_example
         )
+        logger.info("Model logged successfully.")
 
 def main():
-    data_path = Path(DATA_PATH)
+    logger.debug("Starting the training process...")
+    data_path = Path(DATASET_DOWNLOAD_PATH) / "train.csv"
     df = load_data(data_path)
+    df = preprocess_data(df)
     X, y, numeric, boolean, cyclic, categorical = prepare_features(df)
     X_train, X_test, y_train, y_test, preprocessor = split_and_preprocess(X, y, numeric, boolean, cyclic, categorical)
 
