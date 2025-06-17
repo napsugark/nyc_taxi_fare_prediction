63438891858639235f41fdfd5983525abbf08089
Napsugar Kelemen <napsugar.kelemen@qubiz.com>
2025-06-17 15:43:53 +0300

    auto-run

--- src/run_experiment.py
+++ src/run_experiment.py
@@ -1,10 +1,11 @@
 from dotenv import load_dotenv
 import os
 import subprocess
+import argparse
 
 load_dotenv()  # loads AWS credentials
 
-def run_pipeline():
+def run_pipeline(commit_message):
     env = os.environ.copy()  # Get current env, including loaded AWS credentials
     subprocess.run(["dvc", "repro"], check=True, env=env)
     subprocess.run(["dvc", "push"], check=True, env=env)
@@ -13,7 +14,15 @@ def run_pipeline():
 
     
 def main():
-    run_pipeline()
+    parser = argparse.ArgumentParser(description="Run DVC pipeline and push results.")
+    parser.add_argument(
+        "-m", "--message",
+        type=str,
+        default="auto-run",
+        help="Git commit message"
+    )
+    args = parser.parse_args()
+    run_pipeline(args.message)
 
 if __name__ == "__main__":
     main()
